aa7cb0613fea837fb0e60282c60957543f1f4f75
Jeff Nuyts <NUYJE@sintcordula.be>
2024-10-09 12:01:19 +0200

    Melding "Hello World" gewijzigd

--- GithubTestADA/Program.cs
+++ GithubTestADA/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dit is een test met 6ADA");
+
         }
     }
 }
